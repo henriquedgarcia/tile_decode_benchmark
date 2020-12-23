942c3b488d1a31c9adacb62d223a53c6c0c4461a
Henrique D. Garcia <henriquedgarcia@gmail.com>
2020-12-23 19:23:20 -0300

    change projetct name
    Signed-off-by: Henrique D. Garcia <henriquedgarcia@gmail.com>

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from dectime.dectime import Dectime, Role
 
 
 def main():
-    config = f'config/config_user_dectime_9videos_test.json'
+    config = f'config/config_user_dectime_28videos_nas.json'
 
     tile_dectime = Dectime(config)
 
-    # tile_dectime.run(Role.PREPARE)
-    # tile_dectime.run(Role.COMPRESS)
+    tile_dectime.run(Role.PREPARE)
+    tile_dectime.run(Role.COMPRESS)
     tile_dectime.run(Role.SEGMENT)
     # tile_dectime.run(Role.DECODE)
     # tile_dectime.run(Role.RESULTS)
