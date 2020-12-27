7cdf3d1749aa2dabd5f42550b5838fbe90dc58bd
Henrique D. Garcia <henriquedgarcia@gmail.com>
2020-12-26 21:57:30 -0300

    Update main() comands.
    Signed-off-by: Henrique D. Garcia <henriquedgarcia@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dectime.dectime import Dectime, Role
+# from dectime.dectime import CheckDectime
 
 
 def main():
@@ -11,6 +12,11 @@ def main():
     tile_dectime.run(Role.SEGMENT)
     # tile_dectime.run(Role.DECODE)
     # tile_dectime.run(Role.RESULTS)
+
+    # check_files = CheckDectime(
+    #     config=f'config/config_user_dectime_28videos_nas.json')
+    # check_files.check()
+    # check_files.save_report()
 
 
 if __name__ == '__main__':
