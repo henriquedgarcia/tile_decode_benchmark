a5404ae399c781ff095288a950bedfb1dae3a937
Henrique D. Garcia <henriquedgarcia@gmail.com>
2021-01-03 02:46:45 -0300

    Add a loop to decode several times.

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def main():
     tile_dectime.run(Role.PREPARE)
     tile_dectime.run(Role.COMPRESS)
     tile_dectime.run(Role.SEGMENT)
-    # tile_dectime.run(Role.DECODE)
-    # tile_dectime.run(Role.RESULTS)
+    for _ in range(tile_dectime.config.decode_num):
+        tile_dectime.run(Role.DECODE)
+    tile_dectime.run(Role.RESULTS)
 
     # check_files = CheckDectime(
     #     config=f'config/config_user_dectime_28videos_nas.json')
