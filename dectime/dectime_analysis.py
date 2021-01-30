044dc496fdc269b76acf0280f3cc4bec50ebd682
Henrique D. Garcia <henriquedgarcia@gmail.com>
2021-01-29 23:19:28 -0300

    Create a class to make histogram and plot by tiling pattern.

--- dectime/dectime_analysis.py
+++ dectime/dectime_analysis.py
@@ -191,3 +191,270 @@ class BasePlot:
 
         return stats_data1, stats_data2
 
+
+class HistByPattern(BasePlot):
+    def create(self):
+        self.get_data()
+        self.make_fit()
+        self.make_dataframe()
+        self.hist_by_pattern()
+
+    def get_data(self):
+        get_data = self.data_handler.get_data
+        data = {}
+
+        for tilling in self.tiling_list:
+            data[tilling.pattern] = get_data(tiling_list=[tilling])
+
+        self.data = data
+
+    def make_fit(self, overwrite=False):
+        for tiling in self.tiling_list:
+            pattern = tiling.pattern
+            name = f'fitter_time_{pattern}_{self.factor}_{self.bins}bins.pickle'
+            out_file = f'{self.workfolder}/data/{name}'
+
+            if not (os.path.exists(out_file) and not overwrite):
+                print(f'Making fit for {pattern}')
+                fitter = Fitter(self.data[pattern].time,
+                                bins=self.bins,
+                                distributions=self.dist)
+                fitter.fit()
+
+                # Saving fit
+                with open(out_file, 'wb') as fd:
+                    pickle.dump(fitter, fd, pickle.HIGHEST_PROTOCOL)
+
+    def _load_fit(self, pattern: str, out_file=None):
+        name = f'fitter_time_{pattern}_{self.factor}_{self.bins}bins.pickle'
+        out_file = out_file if out_file else f'{self.workfolder}/data/{name}'
+
+        print(f'Loading pickle from {out_file}.')
+        with open(out_file, 'rb') as fd:
+            fitter = pickle.load(fd)
+        self.fitter[pattern] = fitter
+
+        # Calculate error
+        bins = len(fitter.x)
+        df_error = fitter.df_errors['sumsquare_error']
+
+        if self.error_metric is ErrorMetric.RMSE:
+            self.df_error[pattern] = np.sqrt(df_error / bins)
+        elif self.error_metric is ErrorMetric.SSE:
+            self.df_error[pattern] = df_error
+        elif self.error_metric is ErrorMetric.NRMSE:
+            rmse = np.sqrt(fitter.df_errors / bins)
+            err_rg = (fitter.df_errors.max() - fitter.df_errors.min())
+            self.df_error[pattern] = (rmse / err_rg)
+
+    def make_dataframe(self, overwrite=False):
+        exist = (os.path.exists(f'{self.workfolder}/fitted_dataframe.csv') and
+                 os.path.exists(f'{self.workfolder}/stats_dataframe.csv') and
+                 os.path.exists(f'{self.workfolder}/paper_dataframe.csv'))
+        if not (exist and not overwrite):
+            error_metric = self.error_metric.name
+            fitted_dataframe = {
+                'Format': [],
+                'Distribution': [],
+                error_metric: [],
+                'Parameters': [],
+                'Loc': [],
+                'Scale': []}
+            stats_dataframe = {}
+            paper_dataframe = {
+                'Pattern': [],
+                'Mean Time': [],
+                'Deviation Time': [],
+                'Mean Rate': [],
+                'Deviation Rate': [],
+                'Correlation': []}
+
+            for tiling in self.tiling_list:
+                pattern = tiling.pattern
+                self._load_fit(pattern)
+
+                # Register statistics
+                time_stats_, rate_stats_ = self._calc_stats(
+                    self.data[pattern].time, self.data[pattern].rate)
+
+                paper_dataframe['Pattern'].append(pattern)
+                paper_dataframe['Mean Time'].append(time_stats_['average'])
+                paper_dataframe['Deviation Time'].append(time_stats_['std'])
+                paper_dataframe['Mean Rate'].append(rate_stats_['average'])
+                paper_dataframe['Deviation Rate'].append(rate_stats_['std'])
+                paper_dataframe['Correlation'].append(
+                    time_stats_['correlation'])
+
+                time_stats = {f'time_{key}': time_stats_[key]
+                              for key in time_stats_}
+                rate_stats = {f'rate_{key}': rate_stats_[key]
+                              for key in rate_stats_}
+
+                stats_dataframe[pattern] = time_stats.copy()
+                stats_dataframe[pattern].update(rate_stats)
+
+                # Register distributions, parameters and fit errors
+                sorted_errors = (self.df_error[pattern]
+                                 .sort_values())
+                shorted_dist = sorted_errors.index
+
+                for dist in shorted_dist:
+                    try:
+                        params = self.fitter[pattern].fitted_param[dist]
+                    except KeyError:
+                        continue
+
+                    fitted_dataframe['Format'].append(pattern)
+                    if dist in 'burr12':
+                        fitted_dataframe['Distribution'].append('Burr Type XII')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(
+                            f'c={params[0]}, d={params[1]}')
+                        fitted_dataframe['Loc'].append(params[2])
+                        fitted_dataframe['Scale'].append(params[3])
+                    elif dist in 'fatiguelife':
+                        fitted_dataframe['Distribution'].append(
+                            'Birnbaum-Saunders')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'c={params[0]}')
+                        fitted_dataframe['Loc'].append(params[1])
+                        fitted_dataframe['Scale'].append(params[2])
+                    elif dist in 'gamma':
+                        fitted_dataframe['Distribution'].append('Gama')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'a={params[0]}')
+                        fitted_dataframe['Loc'].append(params[1])
+                        fitted_dataframe['Scale'].append(params[2])
+                    elif dist in 'invgauss':
+                        fitted_dataframe['Distribution'].append(
+                            'Inverse Gaussian')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'mu={params[0]}')
+                        fitted_dataframe['Loc'].append(params[1])
+                        fitted_dataframe['Scale'].append(params[2])
+                    elif dist in 'rayleigh':
+                        fitted_dataframe['Distribution'].append('Rayleigh')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'')
+                        fitted_dataframe['Loc'].append(params[0])
+                        fitted_dataframe['Scale'].append(params[1])
+                    elif dist in 'lognorm':
+                        fitted_dataframe['Distribution'].append('Log Normal')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f's={params[0]}')
+                        fitted_dataframe['Loc'].append(params[1])
+                        fitted_dataframe['Scale'].append(params[2])
+                    elif dist in 'genpareto':
+                        fitted_dataframe['Distribution'].append(
+                            'Generalized Pareto')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'c={params[0]}')
+                        fitted_dataframe['Loc'].append(params[1])
+                        fitted_dataframe['Scale'].append(params[2])
+                    elif dist in 'pareto':
+                        fitted_dataframe['Distribution'].append(
+                            'Pareto Distribution')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'b={params[0]}')
+                        fitted_dataframe['Loc'].append(params[1])
+                        fitted_dataframe['Scale'].append(params[2])
+                    elif dist in 'halfnorm':
+                        fitted_dataframe['Distribution'].append('Half-Normal')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'')
+                        fitted_dataframe['Loc'].append(params[0])
+                        fitted_dataframe['Scale'].append(params[1])
+                    elif dist in 'expon':
+                        fitted_dataframe['Distribution'].append('Exponential')
+                        fitted_dataframe[error_metric].append(
+                            self.df_error[pattern][dist])
+                        fitted_dataframe['Parameters'].append(f'')
+                        fitted_dataframe['Loc'].append(params[0])
+                        fitted_dataframe['Scale'].append(params[1])
+                    else:
+                        fitted_dataframe['Distribution'].append(
+                            f'erro: {dist} not found')
+                        fitted_dataframe[error_metric].append(0)
+                        fitted_dataframe['Parameters'].append(f'')
+                        fitted_dataframe['Loc'].append(0)
+                        fitted_dataframe['Scale'].append(0)
+
+            # Create Pandas Dataframe
+            self.fitted_dataframe = pd.DataFrame(fitted_dataframe)
+            self.stats_dataframe = pd.DataFrame(stats_dataframe).T
+            self.paper_dataframe = pd.DataFrame(paper_dataframe)
+            self.paper_dataframe.to_csv(
+                f'{self.workfolder}/paper_dataframe.csv', index=False)
+            self.fitted_dataframe.to_csv(
+                f'{self.workfolder}/fitted_dataframe.csv', index=False)
+            self.stats_dataframe.to_csv(
+                f'{self.workfolder}/stats_dataframe.csv', index_label='pattern')
+
+    def _load_dataframes(self):
+        self.fitted_dataframe = pd.read_csv(
+            f'{self.workfolder}/fitted_dataframe.csv')
+        self.stats_dataframe = pd.read_csv(
+            f'{self.workfolder}/stats_dataframe.csv', index_col=0)
+
+    def _make_hist(self, ax, pattern, n_dist=3):
+        ax.set_title(f'{pattern}')
+        ax.set_xlabel('Decoding Time (s)')
+
+        # ax.hist(self.data[pattern].time,
+        #         bins=self.bins,
+        #         histtype='bar',
+        #         density=True,
+        #         label="Empirical",
+        #         color='#3297c9')
+        #
+        ax.bar(self.fitter[pattern].x, self.fitter[pattern].y,
+               width=self.fitter[pattern].x[1] - self.fitter[pattern].x[0],
+               label='empirical', color='#3297c9', edgecolor='black',
+               linewidth=0.5)
+
+        dist_error = self.df_error[pattern]
+        errors_sorted = dist_error.sort_values()[0:n_dist]
+        c = iter(self.color_list)
+
+        for dist_name in errors_sorted.index:
+            label = f'{dist_name}'
+            ax.plot(self.fitter[pattern].x,
+                    self.fitter[pattern].fitted_pdf[dist_name],
+                    label=label,
+                    color=next(c),
+                    lw=1., )
+
+        ax.legend(loc='upper right')
+
+    def hist_by_pattern(self, name=None, overwrite=False):
+        """Usado no SVR e Electronic Imaging"""
+        name = name if name else f'hist_by_pattern_{self.bins}bins_{self.factor}'
+        filename = f'{self.workfolder}/{name}.png'
+        exist = os.path.exists(filename)
+        if not (exist and not overwrite):
+            self._load_dataframes()
+
+            fig = figure.Figure()
+
+            for n, tilling in enumerate(self.tiling_list, 1):
+                pattern = tilling.pattern
+                self._load_fit(pattern)
+
+                if n > 2 * 4: break
+
+                ax: axes.Axes = fig.add_subplot(2, 4, n)
+                self._make_hist(ax, pattern)
+
+            print(f'Salvando a figura')
+            # fig.show()
+            fig.savefig(filename)
+
