02233dd0af6af89773c1a45ba2d418493578ab7d
Henrique D. Garcia <henriquedgarcia@gmail.com>
2021-01-29 23:17:28 -0300

    Re-made DectimeHandler for get data from json only.

--- dectime/dectime_analysis.py
+++ dectime/dectime_analysis.py
@@ -32,102 +32,52 @@ class DectimeHandler:
              'average_r': 0.0, 'std_r': 0.0, 'percentile_r': percentile.copy(),
              'corr': 0.0, 'fitter': None}
 
-    def __init__(self, config_file, stats_folder='stats',
-                 error_metric='sse', bins: Union[str, int] = "auto"):
-        self.cfg = Config(config_file)
-        self.video_state = VideoState(self.cfg)
-        self.dectime_filename = self.video_state.result_file
+    fit: Fitter
+    data: Data
+    dectime: dict
 
-        self.stats_folder = f'results/{self.cfg.project}/{stats_folder}'
-        os.makedirs(self.stats_folder, exist_ok=True)
+    def __init__(self, config: Config, error_metric='sse',
+                 bins: Union[str, int] = "auto"):
+        self.config = config
+        self.video_state = VideoState(self.config)
+        self.dectime_filename = self.video_state.dectime_raw_json
 
-        self.raw_dectime = {}
-        self.data = {}
-        self.time = []
-        self.rate = []
+        self.work_folder = (f'results/{self.config.project}/'
+                            f'{self.config.stats_folder}')
+        os.makedirs(self.work_folder, exist_ok=True)
 
         # Fitter attributes
-        self.fit_file: Union[str, None] = None
         self.error_metric = error_metric
         self.bins = bins
-        self.fit: Union[fitter.Fitter, None] = None
 
-        self.load_dectime()
+        self.load_dectime(self.dectime_filename)
 
-    def load_dectime(self):
-        with open(self.dectime_filename) as f:
-            self.raw_dectime = json.load(f)
+    def load_dectime(self, dectime_filename):
+        with open(dectime_filename) as f:
+            self.dectime = json.load(f)
 
-    def make_fit(self, time_or_rate, overwrite=False):
-        data_serie = eval(f'self.{time_or_rate}')
-
-        if self.fit_file is None:
-            self.fit_file = f'{self.stats_folder}/' \
-                            f'fitter_pickle_{len(self.time)}.pickle'
-
-        # Calcule fit
-        if os.path.exists(self.fit_file) and not overwrite:
-            with open(self.fit_file, 'rb') as f:
-                self.fit = pickle.load(f)
-        else:
-            self.fit = fitter.Fitter(data_serie, bins=self.bins, timeout=30,
-                                     distributions=self.cfg.distributions)
-            self.fit.fit()
-
-        bins = len(self.fit.x)
-        if self.error_metric in 'sse':
-            pass
-        if self.error_metric in 'rmse':
-            f.df_errors = np.sqrt(f.df_errors / bins)
-        elif self.error_metric in 'nrmse':
-            data_range = (f.df_errors.max() - f.df_errors.min())
-            f.df_errors = (np.sqrt(f.df_errors / bins) / data_range)
-
-    def calc_stats(self):
-        if self.time or self.rate is None:
-            raise ValueError('Atributos time ou rate não definido')
-
-        self.make_fit('time')
-
-        # Calculate percentiles
-        per = [0, 25, 50, 75, 100]
-        percentile = np.percentile([self.time, self.rate], per)
-        percentile_t = {key: value for key, value in
-                        zip(per, percentile[:, 0])}
-        percentile_r = {key: value for key, value in
-                        zip(per, percentile[:, 1])}
-
-        # Struct statistics results
-        self.stats['average_t'] = np.average(self.time)
-        self.stats['std_t'] = float(np.std(self.time))
-        self.stats['average_r'] = np.average(self.rate)
-        self.stats['std_r'] = float(np.std(self.rate))
-        self.stats['corr'] = np.corrcoef((self.time, self.rate))[1][0]
-        self.stats['percentile_t'] = percentile_t
-        self.stats['percentile_r'] = percentile_r
-
-    def _get_data(self, groups: Union[List[int], None] = None,
-                  videos_list: Union[List[Video], None] = None,
-                  pattern_list: Union[List[Pattern], None] = None,
-                  quality_list: Union[List[int], None] = None,
-                  tiles_list: Union[List[Tile], None] = None,
-                  chunks_list: Union[range, None] = None):
+    def get_data(self, groups: Union[List[int], None] = None,
+                 videos_list: Union[List[Video], None] = None,
+                 tiling_list: Union[List[Tiling], None] = None,
+                 quality_list: Union[List[int], None] = None,
+                 tiles_list: Union[List[Tile], None] = None,
+                 chunks_list: Union[range, None] = None) -> Data:
         """
         Pega os dados do json
         :param groups:
         :param videos_list:
-        :param pattern_list:
+        :param tiling_list:
         :param quality_list:
         :param tiles_list:
         :param chunks_list:
         :return: (pd.DataFrame, pd.DataFrame, double)
         """
         if videos_list is None:
-            videos_list = self.cfg.videos_list
-        if pattern_list is None:
-            pattern_list = self.cfg.pattern_list
+            videos_list = self.config.videos_list
+        if tiling_list is None:
+            tiling_list = self.config.tiling_list
         if quality_list is None:
-            quality_list = self.cfg.quality_list
+            quality_list = self.config.quality_list
         if tiles_list is None:
             tiles_list = "pattern.tiles_list"
         else:
@@ -141,79 +91,32 @@ class DectimeHandler:
         rate = []
         for video in videos_list:
             if groups is not None and video.group in groups: continue
-            for pattern in pattern_list:
+            for pattern in tiling_list:
                 for quality in quality_list:
                     for tile in eval(tiles_list):
                         for chunk in eval(chunks_list):
-                            time.append(self.raw_dectime[
-                                            video.name][
-                                            pattern.pattern][
-                                            quality][
-                                            tile.idx][
-                                            chunk]['time'])
+                            time_ = self.dectime[
+                                video.name][
+                                pattern.pattern][
+                                str(quality)][
+                                str(tile.id)][
+                                str(chunk)]['time']
 
-                            rate.append(self.raw_dectime[
-                                            video.name][
-                                            pattern.pattern][
-                                            quality][
-                                            tile.idx][
-                                            chunk]['rate'])
-        return {'time': time, 'rate': rate}
+                            rate_ = self.dectime[
+                                video.name][
+                                pattern.pattern][
+                                str(quality)][
+                                str(tile.id)][
+                                str(chunk)]['rate']
 
-    def get_data_by_pattern(self, pattern_list=None):
-        if pattern_list is None:
-            pattern_list = self.cfg.pattern_list
+                            try:
+                                time_ = time_['avg']
+                            except KeyError:
+                                pass
 
-        for pattern in pattern_list:
-            self.data[pattern.pattern] = self._get_data(pattern_list=[pattern])
+                            time.append(time_)
+                            rate.append(rate_)
 
-    def get_data_by_pattern_quality(self, pattern_list=None, quality_list=None):
-        if pattern_list is None:
-            pattern_list = self.cfg.pattern_list
-        if quality_list is None:
-            quality_list = self.cfg.quality_list
-
-        for pattern in pattern_list:
-            for quality in quality_list:
-                time, rate = self._get_data(pattern_list=[pattern],
-                                            quality_list=[quality])
-                key = f'{pattern.pattern}_{quality}'
-                self.data[key] = dict(time=time,
-                                      rate=rate)
-
-    def get_data_by_video_pattern(self, videos_list=None, pattern_list=None):
-        if videos_list is None:
-            videos_list = self.cfg.videos_list
-        if pattern_list is None:
-            pattern_list = self.cfg.pattern_list
-
-        for video in videos_list:
-            for pattern in pattern_list:
-                time, rate = self._get_data(videos_list=[video],
-                                            pattern_list=[pattern])
-                key = f'{video.name}_{pattern.pattern}'
-                self.data[key] = dict(time=time,
-                                      rate=rate)
-
-    def get_data_by_video_pattern_quality(self, videos_list=None,
-                                          pattern_list=None, quality_list=None):
-        if videos_list is None:
-            videos_list = self.cfg.videos_list
-        if pattern_list is None:
-            pattern_list = self.cfg.pattern_list
-        if quality_list is None:
-            quality_list = self.cfg.quality_list
-
-        for video in videos_list:
-            for pattern in pattern_list:
-                for quality in quality_list:
-                    time, rate = self._get_data(videos_list=[video],
-                                                pattern_list=[pattern],
-                                                quality_list=[quality])
-                    key = f'{video.name}_{pattern.pattern}_{quality}'
-                    self.data[key] = dict(time=time,
-                                          rate=rate)
-
-
+        return DectimeHandler.Data(time, rate)
 
 
