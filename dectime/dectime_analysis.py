791e0e204bc025bca1f5a81dc148a6c3ef9dbd25
Henrique D. Garcia <henriquedgarcia@gmail.com>
2021-03-01 02:05:23 -0300

    Create BasePlot abstract class.

--- dectime/dectime_analysis.py
+++ dectime/dectime_analysis.py
@@ -130,38 +130,51 @@ class DectimeHandler:
         return DectimeData(time, rate)
 
 
-class BasePlot:
+class BasePlot(ABC):
     fitter = AutoDict()
-    data = AutoDict()
     df_error = AutoDict()
+    data = AutoDict()
     colormap = [plt.get_cmap('tab20')(i) for i in range(20)]
     color_list = ("#000000", "#ff8888", "#687964", "#11cc22", "#0f2080",
                   "#ff9910", "#ffc428", "#990099", "#f5793a", "#c9bd9e",
                   "#85c0f9")
-
-    fitted_dataframe: pd.DataFrame
-    stats_dataframe: pd.DataFrame
-    paper_dataframe: pd.DataFrame
     config: Config
 
-    def __init__(self, config: str, folder: str, figsize: Tuple[float, float],
+    def __init__(self, config: str, folder: str,
+                 figsize: Tuple[float, float] = (6.4, 4.8),
                  bins: Union[str, int] = 'auto',
                  error_metric: ErrorMetric = ErrorMetric.RMSE):
         self.error_metric = error_metric
         self.bins = bins
         self.config = Config(config)
+
         self.project: str = self.config.project
         self.data_handler: DectimeHandler = DectimeHandler(self.config)
         self.tiling_list: List[Tiling] = self.config.tiling_list
         self.quality_list: List[int] = self.config.quality_list
         self.factor: str = self.config.factor
         self.dist: List[str] = self.config.distributions
-        os.makedirs(f'{self.workfolder}/data', exist_ok=True)
+
         self.workfolder: str = f'results/{self.project}/graphs/{folder}'
+        os.makedirs(f'{self.workfolder}/data', exist_ok=True)
+
+        self.stats_df = defaultdict(list)
+        self.dist_df: Union[dict, pd.DataFrame] = defaultdict(list)
+        self.paper_df: Dict[Union[int, str], list] = defaultdict(list)
 
         mpl.rc('figure', figsize=figsize, dpi=300, autolayout=True)
         mpl.rc('lines', linewidth=0.5, markersize=3)
+        mpl.rc('patch', linewidth=0.5, edgecolor='black', facecolor='#3297c9')
         mpl.rc('axes', linewidth=0.5, prop_cycle=cycler(color=self.colormap))
+
+    @contextmanager
+    def workfolder_ctx(self, path):
+        my_workfolder = self.workfolder
+        self.workfolder = path
+        try:
+            yield
+        finally:
+            self.workfolder = my_workfolder
 
     @staticmethod
     def _calc_stats(data1, data2):
@@ -193,6 +206,327 @@ class BasePlot:
                            )
 
         return stats_data1, stats_data2
+
+    def _make_dist_df(self,
+                      video_name: Union[str, None] = None,
+                      pattern: Union[str, None] = None,
+                      quality: Union[int, None] = None,
+                      tile: Union[int, None] = None,
+                      chunk: Union[int, None] = None, ):
+        error: pd.Series = dishevel_dictionary(self.df_error, video_name, pattern,
+                                               quality, tile, chunk)
+        dist_list = error.sort_values().index
+
+        for dist in dist_list:
+            fitter: Fitter = dishevel_dictionary(self.fitter, video_name,
+                                                 pattern, quality, tile, chunk)
+            df_error = fitter.df_errors['sumsquare_error']
+            params = fitter.fitted_param[dist]
+            error_metric = df_error[dist]
+            if dist in 'burr12':
+                name = 'Burr Type XII'
+                parameters = f'c={params[0]}, d={params[1]}'
+                loc = params[2]
+                scale = params[3]
+            elif dist in 'fatiguelife':
+                name = 'Birnbaum-Saunders'
+                parameters = f'c={params[0]}'
+                loc = params[1]
+                scale = params[2]
+            elif dist in 'gamma':
+                name = 'Gamma'
+                parameters = f'a={params[0]}'
+                loc = params[1]
+                scale = params[2]
+            elif dist in 'invgauss':
+                name = 'Inverse Gaussian'
+                parameters = f'mu={params[0]}'
+                loc = params[1]
+                scale = params[2]
+            elif dist in 'rayleigh':
+                name = 'Rayleigh'
+                parameters = f''
+                loc = params[0]
+                scale = params[1]
+            elif dist in 'lognorm':
+                name = 'Log Normal'
+                parameters = f's={params[0]}'
+                loc = params[1]
+                scale = params[2]
+            elif dist in 'genpareto':
+                name = 'Generalized Pareto'
+                parameters = f'c={params[0]}'
+                loc = params[1]
+                scale = params[2]
+            elif dist in 'pareto':
+                name = 'Pareto Distribution'
+                parameters = f'b={params[0]}'
+                loc = params[1]
+                scale = params[2]
+            elif dist in 'halfnorm':
+                name = 'Half-Normal'
+                parameters = f''
+                loc = params[0]
+                scale = params[1]
+            elif dist in 'expon':
+                name = 'Exponential'
+                parameters = f''
+                loc = params[0]
+                scale = params[1]
+            else:
+                name = f'erro: {dist} not found'
+                parameters = f''
+                loc = 0
+                scale = 0
+
+            if video_name: self.dist_df['Video Name'].append(video_name)
+            if pattern: self.dist_df['Format'].append(pattern)
+            if quality: self.dist_df['Quality'].append(quality)
+            if tile: self.dist_df['Tile'].append(tile)
+            if chunk: self.dist_df['Chunk'].append(chunk)
+
+            self.dist_df['Distribution'].append(name)
+            self.dist_df[self.error_metric.name].append(error_metric)
+            self.dist_df['Parameters'].append(parameters)
+            self.dist_df['Loc'].append(loc)
+            self.dist_df['Scale'].append(scale)
+
+    def _get_paper_df(self, time_stats, rate_stats,
+                      video_name: Union[str, None] = None,
+                      pattern: Union[str, None] = None,
+                      quality: Union[int, None] = None,
+                      tile: Union[int, None] = None,
+                      chunk: Union[int, None] = None, ):
+        if video_name: self.paper_df['video_name'].append(video_name)
+        if pattern: self.paper_df['pattern'].append(pattern)
+        if quality: self.paper_df['quality'].append(quality)
+        if tile: self.paper_df['tile'].append(tile)
+        if chunk: self.paper_df['chunk'].append(chunk)
+
+        self.paper_df['Mean Time'].append(time_stats['average'])
+        self.paper_df['Deviation Time'].append(time_stats['std'])
+        self.paper_df['Mean Rate'].append(rate_stats['average'])
+        self.paper_df['Deviation Rate'].append(rate_stats['std'])
+        self.paper_df['Correlation'].append(time_stats['correlation'])
+
+    def _get_stats_df(self, time_stats, rate_stats,
+                      video_name: Union[str, None] = None,
+                      pattern: Union[str, None] = None,
+                      quality: Union[int, None] = None,
+                      tile: Union[int, None] = None,
+                      chunk: Union[int, None] = None):
+        if video_name:
+            self.stats_df['video_name'].append(video_name)
+        if pattern:
+            self.stats_df['pattern'].append(pattern)
+        if quality:
+            self.stats_df['quality'].append(quality)
+        if tile:
+            self.stats_df['tile'].append(tile)
+        if chunk:
+            self.stats_df['chunk'].append(chunk)
+
+        time_stats = {f'time_{key}': time_stats[key]
+                      for key in time_stats}
+        rate_stats = {f'rate_{key}': rate_stats[key]
+                      for key in rate_stats}
+        time_stats.update(rate_stats)
+
+        # _ = [self.stats_df[key].append(time_stats[key]) for key in time_stats]
+
+    def make_name(self, base_name, ext: Union[str, None] = None,
+                  video_name: Union[str, None] = None,
+                  pattern: Union[str, None] = None,
+                  quality: Union[int, None] = None,
+                  tile: Union[int, None] = None,
+                  chunk: Union[int, None] = None,
+                  other: Any = None):
+        name = base_name
+        if video_name: name = f'{name}_{video_name}'
+        if pattern: name = f'{name}_{pattern}'
+        if quality: name = f'{name}_{self.factor}{quality}'
+        if tile: name = f'{name}_tile{tile}'
+        if chunk: name = f'{name}_chunk{chunk}'
+        if other: name = f'{name}_{other}'
+        if ext: name = f'{name}.{ext}'
+        return name
+
+    def _load_fit(self, video_name: Union[str, None] = None,
+                  pattern: Union[str, None] = None,
+                  quality: Union[int, None] = None,
+                  tile: Union[int, None] = None,
+                  chunk: Union[int, None] = None,
+                  out_file: str = None):
+        # Create output filename for fitter
+        name = self.make_name('fitter_time', 'pickle', video_name=video_name,
+                              pattern=pattern, quality=quality, tile=tile,
+                              chunk=chunk, other=f'{self.bins}bins')
+        out_file = out_file if out_file else f'{self.workfolder}/data/{name}'
+
+        # Load fitter from pickle
+        print(f'Loading pickle from {out_file}.')
+        with open(out_file, 'rb') as fd:
+            fitter = pickle.load(fd)
+        update_dictionary(fitter, self.fitter, key1=video_name, key2=pattern,
+                          key3=quality, key4=tile, key5=chunk)
+
+        # Calculate error
+        bins = len(fitter.x)
+        df_error = fitter.df_errors['sumsquare_error']
+
+        if self.error_metric is ErrorMetric.RMSE:
+            errors = np.sqrt(df_error / bins)
+        elif self.error_metric is ErrorMetric.SSE:
+            errors = df_error
+        elif self.error_metric is ErrorMetric.NRMSE:
+            rmse = np.sqrt(fitter.df_errors / bins)
+            err_range = (fitter.df_errors.max() - fitter.df_errors.min())
+            errors = (rmse / err_range)
+        else:
+            errors = np.NAN
+
+        update_dictionary(errors, self.df_error, key1=video_name, key2=pattern,
+                          key3=quality, key4=tile, key5=chunk)
+
+    def _make_fit(self, video_name: Union[str, None] = None,
+                  pattern: Union[str, None] = None,
+                  quality: Union[int, None] = None,
+                  tile: Union[int, None] = None,
+                  chunk: Union[int, None] = None,
+                  overwrite=False):
+        name = self.make_name('fitter_time', 'pickle', video_name=video_name,
+                              pattern=pattern, quality=quality, tile=tile,
+                              chunk=chunk, other=f'{self.bins}bins')
+        out_file = f'{self.workfolder}/data/{name}'
+        if os.path.exists(out_file) and not overwrite: return
+
+        print(f'Making fit for {name}')
+        data: DectimeData = dishevel_dictionary(self.data, video_name, pattern,
+                                                quality, tile, chunk)
+        fitter = Fitter(data.time, bins=self.bins, distributions=self.dist)
+        fitter.fit()
+
+        # Saving fit
+        with open(out_file, 'wb') as fd:
+            pickle.dump(fitter, fd, pickle.HIGHEST_PROTOCOL)
+
+    def _make_dataframe(self, video_name: Union[str, None] = None,
+                        pattern: Union[str, None] = None,
+                        quality: Union[int, None] = None,
+                        tile: Union[int, None] = None,
+                        chunk: Union[int, None] = None):
+        self._load_fit(video_name=video_name, pattern=pattern,
+                       quality=quality, tile=tile, chunk=chunk)
+
+        # Calculate statistics
+        data: DectimeData = dishevel_dictionary(self.data, video_name, pattern,
+                                                quality, tile, chunk)
+        time_stats, rate_stats = self._calc_stats(data.time, data.rate)
+
+        # Statistics for paper_df
+        self._get_paper_df(time_stats, rate_stats,
+                           video_name=video_name, pattern=pattern,
+                           quality=quality, tile=tile, chunk=chunk)
+
+        # Update stats dict to stats_df
+        self._get_stats_df(time_stats, rate_stats,
+                           video_name=video_name, pattern=pattern,
+                           quality=quality, tile=tile, chunk=chunk)
+
+        # Register distributions, parameters and fit errors
+        self._make_dist_df(video_name=video_name,
+                           pattern=pattern,
+                           quality=quality,
+                           tile=tile,
+                           chunk=chunk)
+
+    def _save_dataframes(self, stats_dataframe='stats_dataframe',
+                         fitted_dataframe='fitted_dataframe',
+                         paper_dataframe='paper_dataframe'):
+        stats_path = f'{self.workfolder}/{stats_dataframe}.csv'
+        dist_path = f'{self.workfolder}/{fitted_dataframe}.csv'
+        paper_path = f'{self.workfolder}/{paper_dataframe}.csv'
+
+        self.stats_df = pd.DataFrame(self.stats_df)
+        self.stats_df.to_csv(stats_path, index_label='pattern')
+
+        self.dist_df = pd.DataFrame(self.dist_df)
+        self.dist_df.to_csv(dist_path, index=False)
+
+        self.paper_df = pd.DataFrame(self.paper_df)
+        self.paper_df.to_csv(paper_path, index=False)
+
+    def _load_dataframes(self, stats_dataframe='stats_dataframe',
+                         fitted_dataframe='fitted_dataframe',
+                         paper_dataframe='paper_dataframe'):
+        self.stats_df = pd.read_csv(f'{self.workfolder}/{stats_dataframe}.csv',
+                                    index_col=0)
+        self.dist_df = pd.read_csv(f'{self.workfolder}/{fitted_dataframe}.csv')
+        self.paper_df = pd.read_csv(f'{self.workfolder}/{paper_dataframe}.csv')
+
+    def _make_hist(self, ax: axes.Axes,
+                   video_name: Union[str, None] = None,
+                   pattern: Union[str, None] = None,
+                   quality: Union[int, None] = None,
+                   tile: Union[int, None] = None,
+                   chunk: Union[int, None] = None,
+                   n_dist=3,
+                   label='empirical'):
+        color = self.color_list
+        fitter: Fitter = dishevel_dictionary(self.fitter, video_name, pattern,
+                                             quality, tile, chunk)
+        dist_error: pd.Series = dishevel_dictionary(self.df_error, video_name,
+                                                    pattern, quality, tile, chunk)
+        title = self.make_name('histogram', ext=None, video_name=video_name,
+                               pattern=pattern, quality=quality, tile=tile,
+                               chunk=chunk)
+        data: DectimeData = dishevel_dictionary(self.data, video_name, pattern,
+                                                quality, tile, chunk)
+        ax.hist(data.time,
+                bins=self.bins,
+                histtype='bar',
+                density=True,
+                label=label,
+                color='#3297c9')
+
+        # ax.bar(fitter.x, fitter.y,
+        #        width=fitter.x[1] - fitter.x[0],
+        #        label=label)
+
+        dists = dist_error.sort_values()[0:n_dist].index
+
+        for n, dist_name in enumerate(dists):
+            fitted_pdf = fitter.fitted_pdf[dist_name]
+            ax.plot(fitter.x, fitted_pdf,
+                    label=f'{dist_name}',
+                    color=color[n],
+                    lw=1., )
+
+        ax.set_title(f'{title}')
+        ax.set_xlabel('Decoding Time (s)')
+        ax.legend(loc='upper right')
+
+    def create(self):
+        self.get_data()
+        self.make_fit()
+        self.make_dataframe()
+        self.make_plot()
+
+    @abstractmethod
+    def get_data(self):
+        pass
+
+    @abstractmethod
+    def make_fit(self):
+        pass
+
+    @abstractmethod
+    def make_dataframe(self):
+        pass
+
+    @abstractmethod
+    def make_plot(self):
+        pass
 
 
 class HistByPattern(BasePlot):
