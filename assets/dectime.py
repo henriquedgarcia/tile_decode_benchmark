84c0f36596f734d34d01dd270002fd263759124e
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2021-06-12 15:28:24 -0300

    Add a docstring to prepare_videos() method.

--- assets/dectime.py
+++ assets/dectime.py
@@ -71,8 +71,8 @@ class TileDecodeBenchmark:
 
     def prepare_videos(self, overwrite=False) -> None:
         """
-        Prepare video to encode.
-        :param overwrite:
+        Prepare video to encode. Uncompress and unify resolution, frame rate, pixel format.
+        :param overwrite: If True, this method overwrite previous files.
         """
         for _ in self._iterate(deep=1):
             uncompressed_file = self.state.lossless_file
