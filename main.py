0c86bd927e4a4823d6d6178dcdbca037056147cc
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2021-06-28 22:22:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
 import assets.dectime as dt
 import logging
+from datetime import datetime
 
-# logging.basicConfig(level=logging.DEBUG)
-# logging.basicConfig(level=logging.INFO)
-logging.basicConfig(level=logging.WARNING)
+date = str(datetime.now()).replace(':', '-')
+logging.basicConfig(filename=f'{date}.log', level=logging.DEBUG,
+                    format='%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s')
 
 
 def main():
     """Config"""
-    config = f'config/config_nas_cmp.json'
-    # config = f'config/config_nas_erp.json'
+    # config = f'config/config_nas_cmp.json'
+    config = f'config/config_nas_erp.json'
     # config = f'config/config_test.json'
     # config = f'config/config_ffmpeg_crf_12videos_60s.json'
 
     """Tile_dectime"""
-    dt.TileDecodeBenchmark(config).run('PREPARE', overwrite=False)
+    # dt.TileDecodeBenchmark(config).run('PREPARE', overwrite=False)
     dt.TileDecodeBenchmark(config).run('COMPRESS', overwrite=False)
     # dt.TileDecodeBenchmark(config).run('SEGMENT', overwrite=False)
     # dt.TileDecodeBenchmark(config).run('DECODE', overwrite=False)
     # dt.TileDecodeBenchmark(config).run('RESULTS', overwrite=False)
-    # dt.TileDecodeBenchmark(config).run('SITI', overwrite=False)
+    # dt.TileDecodeBenchmark(config).run('SITI', overwrite=False, animate_graph=False, save=True)
 
     # """Check files"""
-    dt.CheckProject(config=config).run('ORIGINAL', rem_error=False)
-    dt.CheckProject(config=config).run('LOSSLESS', rem_error=False)
+    # dt.CheckProject(config=config).run('ORIGINAL', rem_error=False)
+    # dt.CheckProject(config=config).run('LOSSLESS', rem_error=False)
     dt.CheckProject(config=config).run('COMPRESS', rem_error=False)
     # dt.CheckProject(config=config).run('SEGMENT', rem_error=False)
     # dt.CheckProject(config=config).run('DECODE', rem_error=False)
