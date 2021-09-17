5f4370cd471807a2364f99d9d14fe7c31d0d0d42
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2021-09-17 14:38:14 -0300

    Create the class CircularNumber

--- assets/util.py
+++ assets/util.py
@@ -207,3 +207,201 @@ def sobel(frame):
     soby = ndimage.sobel(frame, axis=1)
     sob = np.hypot(sobx, soby)
     return sob
+
+
+class CircularNumber(Real):
+    _value: float
+    ini_value: float
+    end_value: float
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, v):
+        v, t = self._check_cicle(v)
+        self._value = v
+        self.turn += t
+
+    def __init__(self, v: Union[float, int], turn: int = 0, value_range=(0., 360.)):
+        self.turn = turn
+        self.ini_value = value_range[0]
+        self.end_value = value_range[1]
+        self.value = v
+
+    def _check_cicle(self, v: Union[float]):
+        turn = 0
+        while True:
+            if v >= self.end_value:
+                v -= self.end_value
+                turn += 1
+            elif v < self.ini_value:
+                v += self.end_value
+                turn -= 1
+            elif v < self.end_value or v >= self.ini_value:
+                break
+        return v, turn
+
+    def __float__(self):
+        return CircularNumber(float(self.value), turn=self.turn)
+
+    def __trunc__(self) -> Any:
+        return CircularNumber(int(self.value), turn=self.turn)
+
+    def __floor__(self) -> Any:
+        return CircularNumber(math.floor(self.value), turn=self.turn)
+
+    def __ceil__(self) -> Any:
+        return CircularNumber(math.ceil(self.value), turn=self.turn)
+
+    def __round__(self, ndigits=0) -> Any:
+        return CircularNumber(round(len(self), ndigits))
+
+    def __len__(self):
+        return self.turn * self.end_value + self.value
+
+    def __lt__(self, other: Any) -> bool:
+        if isinstance(other, CircularNumber):
+            lt = len(self) < len(other)
+        elif isinstance(other, (float, int)):
+            lt = len(self) < other
+        else:
+            raise TypeError(f"unsupported operand type(s) for <: '{type(self)}' and '{type(other)}'")
+        return lt
+
+    def __le__(self, other: Any) -> bool:
+        if isinstance(other, CircularNumber):
+            lt = len(self) > len(other)
+        elif isinstance(other, (float, int)):
+            lt = len(self) > other
+        else:
+            raise TypeError(f"unsupported operand type(s) for <=: '{type(self)}' and '{type(other)}'")
+        return lt
+
+    def __eq__(self, other):
+        if isinstance(other, CircularNumber):
+            lt = len(self) == len(other)
+        elif isinstance(other, (float, int)):
+            lt = len(self) == other
+        else:
+            raise TypeError(f"unsupported operand type(s) for <=: '{type(self)}' and '{type(other)}'")
+        return lt
+
+    def __abs__(self):
+        return len(self)
+
+    def __floordiv__(self, other: Union[int, float]) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(self) // len(other))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(len(self) // other)
+        else:
+            raise TypeError(f"unsupported operand type(s) for //: '{type(self)}' and '{type(other)}'")
+        return cn
+
+    def __rfloordiv__(self, other: Union[int, float]) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(other) // len(self))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(other // len(self))
+        else:
+            raise TypeError(f"unsupported operand type(s) for //: '{type(other)}' and '{type(self)}'")
+        return cn
+
+    def __mod__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(self) % len(other))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(len(self) % other)
+        else:
+            raise TypeError(f"unsupported operand type(s) for %: '{type(self)}' and '{type(other)}'")
+        return cn
+
+    def __rmod__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(other) % len(self))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(other % len(self))
+        else:
+            raise TypeError(f"unsupported operand type(s) for %: '{type(other)}' and '{type(self)}'")
+        return cn
+
+    def __add__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(self) + len(other))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(len(self) + other)
+        else:
+            raise TypeError(f"unsupported operand type(s) for +: '{type(self)}' and '{type(other)}'")
+        return cn
+
+    def __radd__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(self) + len(other))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(len(self) + other)
+        else:
+            raise TypeError(f"unsupported operand type(s) for +: '{type(other)}' and '{type(self)}'")
+        return cn
+
+    def __mul__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(self) * len(other))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(len(self) * other)
+        else:
+            raise TypeError(f"unsupported operand type(s) for *: '{type(self)}' and '{type(other)}'")
+        return cn
+
+    def __rmul__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(self) * len(other))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(len(self) * other)
+        else:
+            raise TypeError(f"unsupported operand type(s) for *: '{type(other)}' and '{type(self)}'")
+        return cn
+
+    if sys.version_info < (3, 0):
+        def __div__(self, other: Any) -> Any:
+            self.__floordiv__(other)
+
+        def __rdiv__(self, other):
+            self.__rfloordiv__(other)
+
+    def __truediv__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(self) / len(other))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(len(self) / other)
+        else:
+            raise TypeError(f"unsupported operand type(s) for /: '{type(self)}' and '{type(other)}'")
+        return cn
+
+    def __rtruediv__(self, other: Any) -> Any:
+        if isinstance(other, CircularNumber):
+            cn = CircularNumber(len(other) / len(self))
+        elif isinstance(other, (float, int)):
+            cn = CircularNumber(other / len(self))
+        else:
+            raise TypeError(f"unsupported operand type(s) for /: '{type(other)}' and '{type(self)}'")
+        return cn
+
+    def __neg__(self) -> Any:
+        return CircularNumber(-len(self))
+
+    def __pos__(self) -> Any:
+        return CircularNumber(len(self))
+
+    def __pow__(self, exponent: Any) -> Any:
+        return CircularNumber(len(self) ** exponent)
+
+    def __rpow__(self, base: Any) -> Any:
+        return CircularNumber(base ** len(self))
+
+    def __hash__(self) -> int:
+        return hash(self)
+
+    def __repr__(self):
+        return f'{str(self.value)} = {self.turn}x{self.end_value} = {len(self)}'
