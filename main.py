c8532ec0433f4f6b962f36e557803c38378348d9
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2022-01-25 13:05:41 -0300

    Upgrade main interface

--- main.py
+++ main.py
@@ -10,91 +10,98 @@ config_list = []
 # config_list += [f'config/config_nas_erp.json']
 config_list += [f'config/config_test.json']
 # config_list += [f'config/config_ffmpeg_crf_12videos_60s.json']
-x = True
-
 
 def main():
-    decode_time(0, 5)  # 1-pre, 2-com, 3-seg, 4-dec, 5-collect
-    check_files(0, 7)  # 1-ori, 2-loss, 3-comp, 4-seg, 5-clean, 6-dec, 7-res
-    siti(0, 2)
-    make_graphs(0, 6)
-    quality(0, 5)  # 1-all, 2-psnr, 3-wspsnr, 4-spsnr, 5-results
-    tiles_from_dataset(x, 1)  # 1-prepare, 2-get_tile
+    # Main.decode_time(5)  # 1-pre, 2-com, 3-seg, 4-dec, 5-collect
+    # Main.check_files(7)  # 1-ori, 2-loss, 3-comp, 4-seg, 5-clean, 6-dec, 7-res
+    # Main.siti(2)
+    # Main.make_graphs(6)
+    # Main.quality(5)  # 1-all, 2-psnr, 3-wspsnr, 4-spsnr, 5-results
+    # Main.tiles_from_dataset(1)  # 1-prepare, 2-get_tile
+    Main.tiles_from_dataset(2)  # 1-prepare, 2-get_tile
 
-def decode_time(run, role_id, role_end=None):
-    opt = {
-        1: ('PREPARE', dict(overwrite=False)),
-        2: ('COMPRESS', dict(overwrite=False)),
-        3: ('SEGMENT', dict(overwrite=False)),
-        4: ('DECODE', dict(overwrite=False)),
-        5: ('COLLECT_RESULTS', dict(overwrite=False)),
-    }
-    start(run, opt, TileDecodeBenchmark, role_id, role_end)
 
-def check_files(run, role_id, role_end=None):
-    opt = {
-        1: ('CHECK_ORIGINAL', dict(only_error=True, check_video=True,
-                                   check_log=True, clean=False)),
-        2: ('CHECK_LOSSLESS', dict(only_error=True, check_video=True,
-                                   deep_check=True, clean=False)),
-        3: ('CHECK_COMPRESS', dict(only_error=True, check_log=True,
-                                   check_video=True, check_gop=False,
-                                   clean=False)),
-        4: ('CHECK_SEGMENT', dict(only_error=True, check_video=True,
-                                  deep_check=False, clean=False)),
-        5: ('CLEAN', dict(only_error=True, clean_log=True,
-                          clean_video=True, video_of_log=False,
-                          table_of_check=('CHECK_COMPRESS-table-2021-09'
-                                          '-30 15:54:29.901483.csv'),
-                          )),
-        6: ('CHECK_DECODE', dict(only_error=True, clean=False)),
-        7: ('CHECK_RESULTS', dict(only_error=True)),
-    }
-    start(run, opt, CheckTiles, role_id, role_end)
-
-def siti(run, role_id, role_end=None):
-    opt = {
-        # Measure normal SITI
-        1: ('SITI', dict(overwrite=False, animate_graph=False,
-                         save=True)),
-    }
-    start(run, opt, TileDecodeBenchmark, role_id, role_end)
-
-def quality(run, role_id, role_end=None):
-    opt = {
-        1: ('ALL', dict(overwrite=False)),
-        2: ('PSNR', dict(overwrite=False)),
-        3: ('WSPSNR', dict(overwrite=False)),
-        4: ('SPSNR', dict(overwrite=False)),
-        5: ('RESULTS', dict(overwrite=False)),
-    }
-    start(run, opt, QualityAssessment, role_id, role_end)
-
-def tiles_from_dataset(run, role_id, role_end=None):
-    opt = {
-        # Measure normal SITI
-        1: ('PREPARE', dict(overwrite=False,
-                            database_name='nasrabadi_28videos')),
-        2: ('GET_TILES', dict(overwrite=False,
-                              database_name='nasrabadi_28videos',
+class Main:
+    @staticmethod
+    def decode_time(role_ini, role_end=None):
+        opt = {
+            1: ('PREPARE', dict(overwrite=False)),
+            2: ('COMPRESS', dict(overwrite=False)),
+            3: ('SEGMENT', dict(overwrite=False)),
+            4: ('DECODE', dict(overwrite=False)),
+            5: ('COLLECT_RESULTS', dict(overwrite=False)),
+        }
+        Main.start(opt, TileDecodeBenchmark, role_ini, role_end)
+    
+    @staticmethod
+    def check_files(role_ini, role_end=None):
+        opt = {
+            1: ('CHECK_ORIGINAL', dict(only_error=True, check_video=True,
+                                       check_log=True, clean=False)),
+            2: ('CHECK_LOSSLESS', dict(only_error=True, check_video=True,
+                                       deep_check=True, clean=False)),
+            3: ('CHECK_COMPRESS', dict(only_error=True, check_log=True,
+                                       check_video=True, check_gop=False,
+                                       clean=False)),
+            4: ('CHECK_SEGMENT', dict(only_error=True, check_video=True,
+                                      deep_check=False, clean=False)),
+            5: ('CLEAN', dict(only_error=True, clean_log=True,
+                              clean_video=True, video_of_log=False,
+                              table_of_check=('CHECK_COMPRESS-table-2021-09'
+                                              '-30 15:54:29.901483.csv'),
                               )),
-    }
-    start(run, opt, GetTiles, role_id, role_end)
+            6: ('CHECK_DECODE', dict(only_error=True, clean=False)),
+            7: ('CHECK_RESULTS', dict(only_error=True)),
+        }
+        Main.start(opt, CheckTiles, role_ini, role_end)
+    
+    @staticmethod
+    def siti(role_ini, role_end=None):
+        opt = {
+            # Measure normal SITI
+            1: ('SITI', dict(overwrite=False, animate_graph=False,
+                             save=True)),
+        }
+        Main.start(opt, TileDecodeBenchmark, role_ini, role_end)
+    
+    @staticmethod
+    def quality(role_ini, role_end=None):
+        opt = {
+            1: ('ALL', dict(overwrite=False)),
+            2: ('PSNR', dict(overwrite=False)),
+            3: ('WSPSNR', dict(overwrite=False)),
+            4: ('SPSNR', dict(overwrite=False)),
+            5: ('RESULTS', dict(overwrite=False)),
+        }
+        Main.start(opt, QualityAssessment, role_ini, role_end)
+    
+    @staticmethod
+    def tiles_from_dataset(role_ini, role_end=None):
+        opt = {
+            # Measure normal SITI
+            1: ('PREPARE', dict(overwrite=False,
+                                database_name='nasrabadi_28videos')),
+            2: ('GET_TILES', dict(overwrite=False,
+                                  database_name='nasrabadi_28videos',
+                                  )),
+        }
+        Main.start(opt, GetTiles, role_ini, role_end)
+    
+    @staticmethod
+    def make_graphs(role_ini, role_end=None):
+        opt = {
+            1: ('SITI', dict(overwrite=False, animate_graph=False,
+                             save=True)),
+        }
+        Main.start(opt, TileDecodeBenchmark, role_ini, role_end)
 
-def make_graphs(run, role_id, role_end=None):
-    opt = {
-        1: ('SITI', dict(overwrite=False, animate_graph=False,
-                         save=True)),
-    }
-    start(run, opt, TileDecodeBenchmark, role_id, role_end)
-
-def start(run, opt, cls, role_id, role_end = None):
-    if not run: return
-    role_end = role_end if role_end else role_id
-    for i in range(role_id, role_end + 1):
-        role = opt[role_id][0]
-        kwargs = opt[role_id][1]
-        cls(config=config, role=role, **kwargs)
+    @staticmethod
+    def start(opt, cls, role_ini, role_end = None):
+        role_end = role_end if role_end else role_ini
+        for i in range(role_ini, role_end + 1):
+            role = opt[role_ini][0]
+            kwargs = opt[role_ini][1]
+            cls(config=config, role=role, **kwargs)
 
 if __name__ == '__main__':
     import argparse
