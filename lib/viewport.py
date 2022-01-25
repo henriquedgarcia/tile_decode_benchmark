66fb537c543ef4cbc2864286a3d55d91db9fa271
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2022-01-25 13:07:38 -0300

    Update viewport.py

--- lib/viewport.py
+++ lib/viewport.py
@@ -1,24 +1,25 @@
 import numpy as np
-from util import splitx, rot_matrix, proj2sph, hcs2cart, idx2xy
-from assets import Fov, Point_bcs, Point_hcs, Point3d, Resolution, Point2d
+from lib.util import rot_matrix, erp2hcs, hcs2xyz
+from lib.assets import Fov, PointBCS, Resolution, Point, Pixel, PointHCS
 import cv2
-
+import matplotlib.pyplot as plt
+from typing import Union, Iterator
 
 class Plane:
-    normal: Point3d
+    normal: Point
     relation: str
 
-    def __init__(self, normal=Point3d(0, 0, 0), relation='<'):
+    def __init__(self, normal=Point(0, 0, 0), relation='<'):
         self.normal = normal
         self.relation = relation  # With viewport
 
 
 class View:
-    center = Point_hcs(1, 0, 0)
+    center = PointHCS(0, 0)
 
     def __init__(self, fov='0x0'):
         """
-        The viewport is the a region of sphere created by the intersection of
+        The viewport is the region of sphere created by the intersection of
         four planes that pass by center of sphere and the Field of view angles.
         Each plane split the sphere in two hemispheres and consider the viewport
         overlap.
@@ -32,12 +33,12 @@ class View:
         fovx = np.deg2rad(self.fov.H)
         fovy = np.deg2rad(self.fov.W)
 
-        self.p1 = Plane(Point3d(-np.sin(fovy / 2), 0, np.cos(fovy / 2)))
-        self.p2 = Plane(Point3d(-np.sin(fovy / 2), 0, -np.cos(fovy / 2)))
-        self.p3 = Plane(Point3d(-np.sin(fovx / 2), np.cos(fovx / 2), 0))
-        self.p4 = Plane(Point3d(-np.sin(fovx / 2), -np.cos(fovx / 2), 0))
+        self.p1 = Plane(Point(-np.sin(fovy / 2), 0, np.cos(fovy / 2)))
+        self.p2 = Plane(Point(-np.sin(fovy / 2), 0, -np.cos(fovy / 2)))
+        self.p3 = Plane(Point(-np.sin(fovx / 2), np.cos(fovx / 2), 0))
+        self.p4 = Plane(Point(-np.sin(fovx / 2), -np.cos(fovx / 2), 0))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Plane]:
         return iter([self.p1, self.p2, self.p3, self.p4])
 
     def get_planes(self):
@@ -45,8 +46,9 @@ class View:
 
 
 class Viewport:
-    position: Point_bcs
+    position: PointBCS
     projection: np.ndarray
+    resolution: Resolution
 
     def __init__(self, fov: str) -> None:
         """
@@ -57,7 +59,7 @@ class Viewport:
         self.default_view = View(fov)
         self.new_view = View(fov)
 
-    def set_position(self, position: Point_bcs):
+    def set_rotation(self, position: PointBCS):
         """
         Set a new position to viewport using aerospace's body coordinate system
         and make the projection. Return numpy.ndarray.
@@ -65,7 +67,7 @@ class Viewport:
         :return:
         """
         self.position = position
-        self._rotate()
+        self.new_view = self._rotate()
         return self
 
     def _rotate(self):
@@ -74,147 +76,64 @@ class Viewport:
         angles in Z-Y-X order. Refer to Wikipedia.
         :return: A new View object with the new position.
         """
-        view: View = self.default_view
-        new_position: Point_bcs = self.position
-
+        view = self.default_view
+        new_position = self.position
         new_view = View(f'{view.fov}')
         mat = rot_matrix(new_position)
-        new_view.center = Point_hcs(1, new_position[0], new_position[1])
 
         # For each plane in view
         for default_plane, new_plane in zip(view, new_view):
-            normal = default_plane.normal
-            roted_normal = mat @ normal
-            new_plane.normal = Point3d(roted_normal[0], roted_normal[1], roted_normal[2])
+            roted_normal = mat @ tuple(default_plane.normal)
+            new_plane.normal = Point(roted_normal[0], roted_normal[1], roted_normal[2])
 
-        self.new_view = new_view
+        return new_view
 
-    def project(self, scale: str):
-        res = Resolution(*splitx(scale))
-        self.projection = self._project_viewport(res)
-        return self
-
-    def _project_viewport(self, res: Resolution) -> np.ndarray:
+    def project(self, resolution: Union[str, Resolution]) -> np.ndarray:
         """
         Project the sphere using ERP. Where is Viewport the
-        :param res: The resolution of the Viewport
+        :param resolution: The resolution of the Viewport
         :return: a numpy.ndarray with one deep color
         """
-        projection = np.ones(res.shape, dtype=np.uint8) * 255
-        for j, i in res:
-            point_hcs = proj2sph(Point2d(i, j), res)
-            point_cart = hcs2cart(point_hcs)
-            if self.is_viewport(point_cart):
-                projection.itemset((j, i), 0)  # by the docs, it is more efficient than projection[j, i] = 0
-        return projection
+        if isinstance(resolution, str):
+            self.resolution = resolution = Resolution(resolution)
+        elif isinstance(resolution, Resolution):
+            self.resolution = resolution = resolution
 
-    def is_viewport(self, point: Point3d) -> bool:
+        self.projection = np.ones(resolution.shape, dtype=np.uint8) * 255
+
+        with np.nditer(self.projection, op_flags=['readwrite'],
+                       flags=['multi_index']) as it:
+            for cell in it:
+                y, x = it.multi_index
+                sph_point = erp2hcs(Pixel(x=x, y=y), resolution)
+                point_3d = hcs2xyz(sph_point)
+                if self.is_viewport(point_3d):
+                    cell[...] = 0
+        return self.projection
+
+    def is_viewport(self, point: Point) -> bool:
         """
         Check if the plane equation is true to viewport
         x1 * m + y1 * n + z1 * z < 0
         If True, the "point" is on the viewport
+        Obs: is_in só retorna true se todas as expressões forem verdadeiras
         :param point: A 3D Point in the space.
         :return: A boolean
         """
         view = self.new_view
-        for plane in view.get_planes():
+        is_in = True
+        for plane in view:
             result = (plane.normal.x * point.x
                       + plane.normal.y * point.y
                       + plane.normal.z * point.z)
-            if result >=0:
-                return False
-        return True
+            test = (result < 0)
+            is_in = is_in and test
+        return is_in
 
     def show(self) -> None:
-        cv2.imshow('imagem', self.projection)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        plt.imshow(self.projection, cmap='gray')
+        plt.show()
 
     def save(self, file_path):
         cv2.imwrite(file_path, self.projection)
         print('save ok')
-
-
-class Tiling:
-    def __init__(self, tiling: str, proj_res: str, fov: str):
-        self.fov_x, self.fov_y = splitx(fov)
-        self.viewport = Viewport(fov)
-        self.M, self.N = splitx(tiling)
-        self.proj_res = Resolution(proj_res)
-        self.proj_w, self.proj_h = splitx(proj_res)
-        self.total_tiles = round(self.M * self.N)
-        self.tile_w = round(self.proj_res.W / self.M)
-        self.tile_h = round(self.proj_res.H / self.N)
-
-    def __str__(self):
-        return f'({self.M}x{self.N}@{self.proj_res}.'
-
-
-    def get_border(self, idx) -> list:
-        """
-        :param idx: indice do tile.
-        :return: list with all border pixels coordinates
-        """
-        tile_m, tile_n = idx2xy(idx, (self.N, self.M))
-        tile_w = self.tile_w
-        tile_h = self.tile_h
-
-        x_i = tile_w * tile_m  # first row
-        x_f = tile_w * (1 + tile_m) - 1  # last row
-        y_i = tile_h * tile_n  # first line
-        y_f = tile_h * (1 + tile_n) - 1  # last line
-        '''
-        [(x_i, y_i), (x_f, y_f)]
-        plt.imshow(tiling.arr),plt.show
-        '''
-        border = []
-        for x, y in zip(range(x_i, x_f), [y_i] * tile_w):
-            border.append((x, y))
-        for x, y in zip(range(x_i, x_f), [y_f] * tile_w):
-            border.append((x, y))
-        for x, y in zip([x_i] * tile_w, range(y_i, y_f)):
-            border.append((x, y))
-        for x, y in zip([x_f] * tile_w, range(y_i, y_f)):
-            border.append((x, y))
-
-        border = list(zip(range(x_i, x_f), [y_i] * tile_w))  # upper border
-        border.extend(list(zip(range(x_i, x_f), [y_f] * tile_w)))  # botton border
-        border.extend(list(zip([x_i] * tile_w, range(y_i, y_f))))  # left border
-        border.extend(list(zip([x_f] * tile_w, range(y_i, y_f))))  # right border
-
-        return border
-
-    def get_vptiles(self, position: Point_bcs):
-        """
-        1. seta o viewport na posição position
-        2. para cada 'tile'
-        2.1. pegue a borda do 'tile'
-        2.2. para cada 'ponto' da borda
-        2.2.1. se 'ponto' pertence ao viewport
-        2.2.1.1. marcar tile
-        2.2.1.2. break
-        3. retorna tiles marcados
-        """
-        self.viewport.set_position(position)
-        tiles = []
-        for idx in range(self.total_tiles):
-            border = self.get_border(idx)
-            for (x, y) in border:
-                point = self._unproject(Point2d(x, y))
-
-                if self.viewport.is_viewport(point):
-                    tiles.append(idx)
-                    break
-        return tiles
-
-    def _unproject(self, point: Point2d):
-        """
-            Convert a 2D point of ERP projection coordinates to Horizontal Coordinate
-            System (Only ERP Projection)
-            :param point: A point in ERP projection
-            :return: A 3D Point on the sphere
-            """
-        proj_scale = Resolution(f'{self.proj_res}')
-        point_hcs = proj2sph(point, proj_scale)
-        point = hcs2cart(point_hcs)
-        return point
