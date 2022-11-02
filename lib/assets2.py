ec771ec1323194cab26a355adec8b4d1acd2802c
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2022-11-02 13:28:44 -0300

    Add colors for print

--- lib/assets2.py
+++ lib/assets2.py
@@ -242,3 +242,14 @@ class Base:
         self.operations[role.name].config = Config(Path(conf))
         self.operations[role.name]()
         print(f'\n====== The end of {role.name} ======')
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
