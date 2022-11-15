ca9758339c55ff099669a5fd37cccb11339e1f86
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2022-11-15 16:24:22 -0300

    Create function position2trajectory()

--- lib/util2.py
+++ lib/util2.py
@@ -1,11 +1,13 @@
 import json
+import pickle
 from logging import warning
 from pathlib import Path
 from subprocess import run, STDOUT
 from typing import Union
-import pickle
 
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 class AutoDict(dict):
@@ -25,9 +27,11 @@ def load_json(filename, object_hook=dict):
         results = json.load(f, object_hook=object_hook)
     return results
 
+
 def save_pickle(data: object, filename: Union[str, Path]):
     with open(filename, 'wb') as f:
         pickle.dump(data, f, protocol=-1)
+
 
 def load_pickle(filename):
     with open(filename, 'rb') as f:
@@ -80,3 +84,70 @@ def idx2xy(idx: int, shape: tuple):
     tile_x = idx % shape[1]
     tile_y = idx // shape[1]
     return tile_x, tile_y
+
+
+def position2trajectory(positions_list, fps=30):
+    # in rads
+    yaw_state = 0
+    pitch_state = 0
+    old_yaw = 0
+    old_pitch = 0
+    yaw_velocity = pd.Series(dtype=float)
+    pitch_velocity = pd.Series(dtype=float)
+    yaw_trajectory = []
+    pitch_trajectory = []
+    pi = np.pi
+
+    for frame, position in enumerate(positions_list):
+        """
+        position: pd.Serie
+        position.index == []
+        """
+        yaw = position[0]
+        pitch = position[1]
+        roll =  position[2]
+
+        if not frame == 1:
+            yaw_diff = yaw - old_yaw
+            if yaw_diff > 1.45:
+                yaw_state -= 1
+            elif yaw_diff < -200:
+                yaw_state += 1
+
+            pitch_diff = pitch - old_pitch
+            if pitch_diff > 120:
+                pitch_state -= 1
+            elif pitch_diff < -120:
+                pitch_state += 1
+            # print(f'Frame {n}, old={old:.3f}°, new={position:.3f}°, diff={diff :.3f}°')  # Want a log?
+
+        new_yaw = yaw + pi * yaw_state
+        yaw_trajectory.append(new_yaw)
+
+        new_pitch = pitch + pi/2 * pitch_state
+        pitch_trajectory.append(new_pitch)
+
+        if frame == 1:
+            yaw_velocity.loc[frame] = 0
+            pitch_velocity.loc[frame] = 0
+        else:
+            yaw_velocity.loc[frame] = (yaw_trajectory[-1] - yaw_trajectory[-2]) * fps
+            pitch_velocity.loc[frame] = (pitch_trajectory[-1] - pitch_trajectory[-2]) * fps
+
+        old_yaw = yaw
+        old_pitch = pitch
+
+    # Filter
+    padded_yaw_velocity = [yaw_velocity.iloc[0]] + list(yaw_velocity) + [yaw_velocity.iloc[-1]]
+    yaw_velocity_filtered = [sum(padded_yaw_velocity[idx - 1:idx + 2]) / 3
+                             for idx in range(1, len(padded_yaw_velocity) - 1)]
+
+    padded_pitch_velocity = [pitch_velocity.iloc[0]] + list(pitch_velocity) + [pitch_velocity.iloc[-1]]
+    pitch_velocity_filtered = [sum(padded_pitch_velocity[idx - 1:idx + 2]) / 3
+                               for idx in range(1, len(padded_pitch_velocity) - 1)]
+
+    # Scalar velocity
+    yaw_speed = np.abs(yaw_velocity_filtered)
+    pitch_speed = np.abs(pitch_velocity_filtered)
+
+
